026be8be64d2ee000b330a9435bb78b5d64e87a7
Max Watermolen <mwateremolen@maxwelldps.com>
2021-11-22 21:55:12 -0600

    formated & implmented stores, and created TR json serilizer

--- trunkplayerNG/radio/transmission.py
+++ trunkplayerNG/radio/transmission.py
@@ -1,5 +1,20 @@
+import base64
+from utils import TransmissionDetails
+
+
 def new_transmission_handler(data):
-    system = data["system"]
-    recorder = data["system"]
-    json = data["system"]
-    audio = data["system"]
+    recorder = data["recorder"]
+    json = data["json"]
+    audio = data["audioFile"]
+
+    Payload: TransmissionDetails = TransmissionDetails(json)
+    audio_bytes: str = base64.b64decode(audio).decode()
+
+    Payload = Payload._to_json()
+
+    Payload["recorder"] = recorder
+    Payload["audioFile"] = audio_bytes
+
+    
+
+    return Payload
