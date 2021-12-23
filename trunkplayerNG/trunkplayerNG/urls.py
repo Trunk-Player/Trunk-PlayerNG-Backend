b3500ca63da44d7dfabb077bcac02bce4a5f032d
Max Watermolen <mwateremolen@maxwelldps.com>
2021-12-22 19:20:22 -0600

    Fixes auth bug

--- trunkplayerNG/trunkplayerNG/urls.py
+++ trunkplayerNG/trunkplayerNG/urls.py
@@ -41,6 +41,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("radio.urls"), name="Radio"),
+    path("api/users/", include("users.urls"), name="Radio"),
     path("api/auth/", include("dj_rest_auth.urls")),
     path("api/registration/", include("dj_rest_auth.registration.urls")),
     url(r"^account/", include("allauth.urls")),
