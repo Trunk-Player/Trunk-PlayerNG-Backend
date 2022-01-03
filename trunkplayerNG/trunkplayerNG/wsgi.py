d48998d3bffb1becd73a711b6be5bc23e599afcf
Max Watermolen <maxw@maxwelldps.com>
2022-01-03 17:13:14 +0000

    inital pruning and socket.io

--- trunkplayerNG/trunkplayerNG/wsgi.py
+++ trunkplayerNG/trunkplayerNG/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
+import gevent.monkey
+gevent.monkey.patch_all()
 from django.core.wsgi import get_wsgi_application
+import socketio
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trunkplayerNG.settings")
 
 application = get_wsgi_application()
+sio = socketio.Server(async_mode='gevent_uwsgi')
+application = socketio.WSGIApp(sio, application)
