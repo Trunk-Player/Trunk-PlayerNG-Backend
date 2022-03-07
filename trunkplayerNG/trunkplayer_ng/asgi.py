13215d52521055d831a905f83aeb090f57611bf9
Max Watermolen <maxw@maxwelldps.com>
2022-03-07 03:58:46 +0000

    pylint fixes

--- trunkplayerNG/trunkplayer_ng/asgi.py
+++ trunkplayerNG/trunkplayer_ng/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for trunkplayerNG project.
+ASGI config for trunkplayer_ng project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trunkplayerNG.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trunkplayer_ng.settings")
 
 application = get_asgi_application()
