38de804c400c047b754e916112bfcb51179ac67b
Max Watermolen <mwateremolen@maxwelldps.com>
2022-07-05 17:26:40 -0500

    adds first test

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ docker run --privileged --rm tonistiigi/binfmt --install all
 docker buildx create --name mybuilder
 docker buildx use mybuilder
 docker buildx inspect --bootstrap
-docker buildx build --platform linux/amd64,linux/arm64 -t trunkplayer/trunkplayer-ng-api:latest --push .
-#docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t trunkplayer/trunkplayer-ng-api:latest --push .
+#docker buildx build --platform linux/amd64,linux/arm64 -t trunkplayer/trunkplayer-ng-api:latest --push .
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t trunkplayer/trunkplayer-ng-api:latest --push .
 docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t trunkplayer/trunkplayer-ng-nginx:latest --push nginx/
 docker buildx rm  --builder mybuilder
