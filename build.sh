bfdbd8196b30daa0720f06bc9651608b059d89fe
Max Watermolen <maxw@maxwelldps.com>
2024-02-09 15:57:34 -0800

    updates

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ docker run --privileged --rm tonistiigi/binfmt --install all
 docker buildx create --name mybuilder
 docker buildx use mybuilder
 docker buildx inspect --bootstrap
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t trunkplayer/trunkplayer-ng-api:latest --push .
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t trunkplayer/trunkplayer-ng-nginx:latest --push nginx/
+docker buildx build --platform linux/amd64,linux/ppc64le,linux/s390x,linux/arm64,linux/arm64/v8,linux/arm/v7,linux/arm/v6,linux/aarch64 \
+    -t trunkplayer/trunkplayer-ng-api:latest -t trunkplayer/trunkplayer-ng-api:$1 --push .
+docker buildx build --platform linux/amd64,linux/ppc64le,linux/s390x,linux/arm64,linux/arm64/v8,linux/arm/v7,linux/arm/v6,linux/aarch64 \
+    -t trunkplayer/trunkplayer-ng-nginx:latest -t trunkplayer/trunkplayer-ng-nginx:$1 --push nginx/
 docker buildx rm  --builder mybuilder
